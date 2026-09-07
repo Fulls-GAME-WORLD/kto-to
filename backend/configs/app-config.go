3965f476aa755319008a52470d65e02df5508f14
pablaofficeal <pavlogorosko56@gmail.com>
2026-09-07 16:02:30 +0200

    feat: add GORM and PostgreSQL driver dependencies and update core packages

--- backend/configs/app-config.go
+++ backend/configs/app-config.go
@@ -1,11 +1,26 @@
 package configs
 
+import (
+	"os"
+)
+
 func GetAppPort() string {
 	SERVER_PORT := "8889"
 	return ":" + SERVER_PORT
 }
 
 func GetDBUrl() string {
-	DB_URL := "postgres://postgres:postgres@localhost:19930/kak_to?sslmode=disable"
+	DB_URL := os.Getenv("DB_URL")
+	if DB_URL == "" {
+		return DevGetDBUrl()
+	}
 	return DB_URL
+}
+
+func GetJWTSecret() string {
+	JWT_SECRET := os.Getenv("JWT_SECRET")
+	if JWT_SECRET == "" {
+		return DevGetJWTSecret()
+	}
+	return JWT_SECRET
 }
